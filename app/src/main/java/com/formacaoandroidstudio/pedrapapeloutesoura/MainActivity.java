118fd69c357558cff263ef2dd82bc5a1b6d28e60
Daniel Penelva <d4n.andrade@gmail.com>
2023-03-30 03:33:53 -0300

    Criando as config. da escolha pedra, papel ou tesoura do app

--- app/src/main/java/com/formacaoandroidstudio/pedrapapeloutesoura/MainActivity.java
+++ app/src/main/java/com/formacaoandroidstudio/pedrapapeloutesoura/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,21 +17,54 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /* O parâmetro View é obrigatório! */
-    public void selecionadoPedra(View view){
+    public void selecionadoPedra(View view) {
 
         this.opcaoSelecionado("pedra");
     }
 
-    public void selecionadoPapel(View view){
+    public void selecionadoPapel(View view) {
 
         this.opcaoSelecionado("papel");
     }
 
-    public void selecionadoTesoura(View view){
+    public void selecionadoTesoura(View view) {
         this.opcaoSelecionado("tesoura");
     }
 
-    public void opcaoSelecionado(String opcaoSelecionada){
-        System.out.println( opcaoSelecionada );
+    public void opcaoSelecionado(String opcaoSelecionada) {
+
+        /* Capturando o id da imagem_padrao */
+        ImageView imageResultado = findViewById(R.id.imageResultado);
+
+        int numero = new Random().nextInt(3);
+
+        /* Opções selecionadas pelo usuário */
+        String[] opcoes = {"pedra", "papel", "tesoura"};
+
+        /* Opções selecionadas pelo app */
+        String opcaoApp = opcoes[numero];
+
+        switch (opcaoApp) {
+
+            case "pedra":
+
+                /* setImageResource é para configurar o recebimento de uma nova imagem através de um recurso.
+                 * Essa imagem vai ser retirada da pasta res > drawable */
+
+                imageResultado.setImageResource(R.drawable.pedra);
+
+                break;
+
+            case "papel":
+                imageResultado.setImageResource(R.drawable.papel);
+                break;
+
+            case "tesoura":
+                imageResultado.setImageResource(R.drawable.tesoura);
+                break;
+        }
+
+        System.out.println("item clicado: " + opcaoApp);
+
     }
 }
